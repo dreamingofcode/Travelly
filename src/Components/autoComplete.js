7276cd7c1b0e610afcac1f0768000238434f0982
Cesar <cesarmota@ymail.com>
2020-09-12 21:43:25 -0500

    checking bug

--- src/Components/autoComplete.js
+++ src/Components/autoComplete.js
@@ -1,83 +1,89 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import AsyncSelect from 'react-select/async';
-import makeAnimated from 'react-select/animated';
-//  import { colourOptions } from '../data';
-import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
+import _ from 'lodash';
 
-const animatedComponent = makeAnimated();
-
-
-export default class AutoComplete extends PureComponent {
-  
-  state = { selectedCity: [] };
-  onChange = (selectedCity) => {
-    this.setState({
-      selectedCity: selectedCity || [],
-    });
-  };
-  loadOptions = async (inputText, callback) => {
-    const configObj = {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
-        'x-rapidapi-key': '78658dd993msha58b4f039c6c59ep11289djsn173e61927b34',
-      },
+class AutoComplete extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      selectedOption: {},
+      loaded: false,
     };
-    const response = await fetch(
-      `https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=10&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=chicago`,
-      configObj
-    );
-    const json = await response.json();
-    console.log(json);
-    callback(
-      json.data.map((i) => ({
-        label: i.result_object.name,
-        value: i.result_object.location_id,
-      }))
-    );
+    // this.getOptions = _.debounce(this.getOptions.bind(this), 500);
+  }
 
-    // setTimeout(() => {
-    // callback(this.setResults(json));
-    // }, 10000);
-  };
-
-  // renderEveryTrip = (trip) => {
-  //   return <p>{trip}</p>;
-  // };
-
-  //   handleInputChange = (newValue: string) => {
-  //     const inputValue = newValue.replace(/\W/g, '');
-  //     this.setState({ inputValue });
-  //     return inputValue;
-  //   };
-  render() {
-    return (
-      <div className="trips">
-        {/* <pre>inputValue: {this.state.inputValue}</pre> */}
+  fetchData = (inputValue, callback) => {
+    if (!inputValue) {
+      callback([]);
+    } else {
+      setTimeout(() => {
+        const configObj = {
+          method: 'GET',
+          headers: {
+            'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
+            'x-rapidapi-key':
+            '78658dd993msha58b4f039c6c59ep11289djsn173e61927b34',
+          },
+        };
+        // !this.state.loaded
+        //   ?
+        fetch(
+          `https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=10&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=${inputValue}`,
+          configObj
+          )
+          .then((resp) => resp.json())
+          .then((resp) => {
+            resp.data
+            ? this.setState({ ...this.state, loaded: !this.state.loaded })
+            : console.log();
+            console.log(
+              resp.data[0].result_object.name,
+              resp.data[0].result_object.location_id
+              );
+              const tempArray = [];
+              resp.data.forEach((element) => {
+                tempArray.push({
+                  value: element.result_object.location_id,
+                  label: element.result_object.name,
+                });
+              });
+              console.log(tempArray);
+              callback(tempArray);
+              // return tempArray
+            })
+            .catch((error) => {
+              console.log(error, 'catch the hoop');
+            });
+            // : console.log();
+          });
+        }
+      };
+      
+      onSearchChange = (selectedOption) => {
+        if (selectedOption) {
+          this.setState({
+            selectedOption,
+          });
+        }
+      };
+      render() {
+        const options=["look","map","turtle"]
+        return (
+          <div>
         <AsyncSelect
-          isMulti
-          components={animatedComponent}
-          value={this.state.selectedCity}
-          onChange={this.onChange}
-          placeholder={'Find City..'}
-          loadOptions={this.loadOptions}
-          // theme={(theme) => ({
-          //   ...theme,
-          //   borderRadius: 0,
-          //   colors: {
-          //     ...theme.colors,
-          //     primary25: 'green',
-          //     primary: 'black',
-          //     neutral0: '#c8c8c8',
-          //     neutral90: '',
-          //   },
-          // })}
-          cacheOptions
-          // loadOptions={loadOptions}
-          // defaultOptions
-          // onInputChange={this.handleInputChange}
+        cacheOptions
+          value={this.state.selectedOption}
+          defaultOptions={options}
+          loadOptions={this.fetchData}
+          placeholder="Admin Name"
+          onChange={(e) => {
+            this.onSearchChange(e);
+          }}
+          defaultOptions={false}
         />
       </div>
     );
   }
 }
+
+export default AutoComplete;
