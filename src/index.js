acf8428227dc9214e61f109759bbe70968155914
Cesar <cesarmota@ymail.com>
2020-09-21 15:04:22 -0500

    redux dev tools second attmept

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,compose } from 'redux';
 import { Provider } from 'react-redux';
 import allReducers from './reducers/index';
 import thunk from 'redux-thunk';
